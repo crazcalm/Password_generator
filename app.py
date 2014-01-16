1b4dfd7ba85bffb60554716066175d3dfbead335
Marcus Willock <crazcalm@gmail.com>
2014-01-15 20:15:10 -0500

    basic flask setup

---
+++ app.py
@@ -0,0 +1,29 @@
+import flask, flask.views
+
+app = flask.Flask(__name__)
+
+app.secret_key = "asndkva;vb ;v vvfvk v ffv464v16fv16fv1"
+
+class Page1(flask.views.MethodView):
+	def get(self):
+		
+		return flask.render_template("base.html")
+
+	def post(self):
+		
+		test = flask.request.form
+
+		for key, value in test.iteritems():
+			print key, value
+		
+		flask.flash(test)
+		#return self.get()
+		return "Working on it..."
+
+	
+app.add_url_rule("/Password_Generator", view_func=Page1.as_view("base"),
+				methods=["GET", "POST"])
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
