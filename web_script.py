7b9d84a6de111a086b8d3c0b532679ea2e2a5188
Marcus Willock <crazcalm@gmail.com>
2014-01-16 18:00:12 -0500

    Coding on the train... untested code

--- web_script.py
+++ web_script.py
@@ -1,6 +1,25 @@
-import string
-from pass_gen2 import num_of_digits, password_chars
+"""Notes:
 
+The Web interface will send a JSON object with
+three entries: {number:X, special: True, digits: y}
+
+Plan:
+
+I need to finish pass_gen2.py first. I forgot to use the 
+num_of_digits function!!! 
+
+It is not needed. The string.printable set includes numbers.
+That is why i never used it (or noticed that I didn't use it).
+I will deleate that function when I get a chance.
+
+I need to take that option out of my web app interface!
+"""
+
+
+import string, random
+from pass_gen2 import password_chars
+
+# Need to test!!!
 def main(pass_len, special_chars, num_digits):
 
 	password = ""
@@ -11,5 +30,13 @@ def main(pass_len, special_chars, num_digits):
 	else:
 		sample_set = string.printable[:62]
 
+	password += random.choice(string.digits)
 	
-	
+	password = pawword_chars(password, pass_len, sample_set)
+
+	password = "".join(random.sample(password, pass_len))
+
+	return password
+
+if __name__ == "__main__":
+	main()
